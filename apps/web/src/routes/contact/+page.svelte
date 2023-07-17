1df33ff1ce6b8141b3b69efe6e0d37a14cf2e274
slekup <74949101+slekup@users.noreply.github.com>
2023-07-18 03:10:07 +1200

    style: make contact page responsive

--- apps/web/src/routes/contact/+page.svelte
+++ apps/web/src/routes/contact/+page.svelte
@@ -17,17 +17,17 @@
 <div class="2xl:py-32 lg:py-20 p-0 bg-default">
   <div class="max-w-5xl mx-auto lg:w-11/12 md:flex lg:rounded-3xl lg:shadow-lg">
     <div
-      class="w-1/3 px-10 py-20 bg-secondary lg:border border-border border-r-0 lg:rounded-l-3xl text-white"
+      class="md:w-1/3 px-10 md:px-5 lg:px-10 py-10 md:py-20 bg-secondary lg:rounded-l-lg text-white"
     >
       <h3 class="font-bold text-2xl">Contact Us</h3>
       <div class="mt-5 flex">
-        <div class="rounded-3xl bg-white/20 p-2 mr-3">
-          <Icon icon="mdi:phone" class="min-h-5 min-w-5 max-w-5 max-h-5" />
+        <div class="rounded-3xl bg-white/20 p-2 mr-3 h-9 w-9">
+          <Icon icon="mdi:phone" class="w-5 h-5" />
         </div>
         <p class="mt-1.5"><b>Phone:</b> +64 27 000 000</p>
       </div>
       <div class="mt-5 flex">
-        <div class="rounded-3xl bg-white/20 p-2 mr-3">
+        <div class="rounded-3xl bg-white/20 p-2 mr-3 h-9 w-9">
           <Icon icon="eva:email-outline" class="h-5 w-5" />
         </div>
         <p class="mt-1.5">
@@ -38,7 +38,7 @@
         </p>
       </div>
       <div class="mt-5 flex">
-        <div class="rounded-3xl bg-white/20 p-2 mr-3">
+        <div class="rounded-3xl bg-white/20 p-2 mr-3 h-9 w-9">
           <Icon icon="gg:website" class="h-5 w-5" />
         </div>
         <p class="mt-1.5">
@@ -47,9 +47,7 @@
       </div>
     </div>
 
-    <div
-      class="w-2/3 px-10 py-20 lg:border border-border lg:rounded-r-3xl bg-background"
-    >
+    <div class="md:w-2/3 px-10 py-20 lg:rounded-r-lg bg-background">
       <h3 class="font-bold text-2xl">Get In Touch</h3>
       <form class="mt-5" on:submit={onSubmit}>
         <input
