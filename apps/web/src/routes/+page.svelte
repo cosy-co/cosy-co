3638289e429ba03fd0469234b82edc939ef8207f
slekup <74949101+slekup@users.noreply.github.com>
2023-07-18 03:42:33 +1200

    style: make homepage responsive

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -4,23 +4,26 @@
 
 <Meta title="Home" />
 
-<div class="relative py-40 w-full">
+<div class="relative py-24 lg:py-32 xl:py-40 w-full">
   <img
     src="/home/banner-1.jpg"
     alt="Home banner"
     class="absolute top-0 left-0 w-full h-full object-cover"
   />
-  <div class="max-w-7xl mx-auto w-11/12 text-white drop-shadow-lg">
+  <div class="max-w-7xl mx-auto w-10/12 text-white drop-shadow-lg">
     <div class="max-w-2xl relative">
-      <h1 class="font-extrabold text-6xl">Embrace the Warmth,<br />Embrace Cozy Co!</h1>
-      <p class="text-lg my-10">
-        Where comfort meets self-care. Discover our premium wheat bags and indulgent products
-        designed to help you unwind, relax, and rejuvenate. Treat yourself to ultimate coziness and
-        embrace a lifestyle of well-being.
+      <h1 class="font-extrabold text-4xl sm:text-5xl lg:text-6xl">
+        Embrace the Warmth,<br />Embrace Cozy Co!
+      </h1>
+      <p class="lg:text-lg my-10">
+        Where comfort meets self-care. Discover our premium wheat bags and
+        indulgent products designed to help you unwind, relax, and rejuvenate.
+        Treat yourself to ultimate coziness and embrace a lifestyle of
+        well-being.
       </p>
       <Button size="lg">Explore More</Button>
     </div>
   </div>
 </div>
 
-<div class="max-w-7xl mx-auto w-11/12">content</div>
+<div class="max-w-7xl mx-auto w-11/12" />
