f22866857faac4909d81091c9f0587ca269f799b
slekup <74949101+slekup@users.noreply.github.com>
2023-07-26 23:10:21 +1200

    build: add all platforms for docker hub image

--- apps/api/.eslintrc.cjs
+++ apps/api/.eslintrc.cjs
@@ -1,3 +1,19 @@
+const path = require('path');
+
 module.exports = {
+  root: true,
   extends: ['custom'],
+  parserOptions: {
+    project: [path.resolve(__dirname, '.tsconfig.json')],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: [path.resolve(__dirname, '.tsconfig.json')],
+      },
+      node: true,
+    },
+  },
 };
