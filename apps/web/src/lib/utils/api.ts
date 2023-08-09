42ba241c4c24735b4fe533ba855da5cba92152c4
slekup <74949101+slekup@users.noreply.github.com>
2023-08-10 01:48:20 +1200

    fix: eslint error

--- apps/web/src/lib/utils/api.ts
+++ apps/web/src/lib/utils/api.ts
@@ -4,7 +4,7 @@ import { Client } from '@cosy-co/api-client';
 import { PUBLIC_API_URL } from '$env/static/public';
 
 const api = new Client({
-  apiUrl: PUBLIC_API_URL,
+  apiUrl: String(PUBLIC_API_URL),
 });
 
 export default api;
